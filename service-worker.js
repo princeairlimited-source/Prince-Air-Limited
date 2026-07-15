3339f1a66412e30d8c3b98f80a3dc94960a36f28
Prince Air Ltd <princeairlimited@gmail.com>
2026-07-15 15:43:36 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,7 @@
-const CACHE_NAME = "prince-air-v1";
 
 const urlsToCache = [
   "/",
   "/index.html",
   "/style.css",
   "/manifest.json"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+]
